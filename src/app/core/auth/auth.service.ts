b9785bd9bcd9d6232aea91ea709e054845f0abdf
Vincent Laude <vincent.laude2@gmail.com>
2020-09-17 16:54:54 +0200

    feat(login): snackbar on login errors

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -6,6 +6,9 @@ import { auth } from 'firebase/app';
   providedIn: 'root',
 })
 export class AuthService {
+  public readonly WRONG_PASSWORD_CODE = 'auth/wrong-password';
+  public readonly USER_NOT_FOUND_CODE = 'auth/user-not-found';
+
   constructor(private afAuth: AngularFireAuth) {}
 
   loginWithEmailAndPassword(email: string, password: string) {
