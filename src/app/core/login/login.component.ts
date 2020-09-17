efd637bf47bc92b1fe2143c61fe867a194c97884
Vincent Laude <vincent.laude2@gmail.com>
2020-09-17 16:54:54 +0200

    feat(login): snackbar on login errors

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../services/snackbar.service';
 
 @Component({
   selector: 'vl-login',
@@ -11,7 +13,12 @@ export class LoginComponent implements OnInit {
   public signInForm: FormGroup;
   public registerForm: FormGroup;
 
-  constructor(public authService: AuthService, private fb: FormBuilder) {}
+  constructor(
+    public authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+    private readonly snackbarService: SnackbarService
+  ) {}
 
   ngOnInit(): void {
     this.initSignInForm();
@@ -22,7 +29,21 @@ export class LoginComponent implements OnInit {
       return;
     }
     const { email, password } = this.signInForm.value;
-    this.authService.loginWithEmailAndPassword(email, password).then(console.log).catch(console.error);
+    this.authService
+      .loginWithEmailAndPassword(email, password)
+      .then((_) => {
+        this.router.navigate(['']);
+      })
+      .catch((error) => {
+        switch (error.code) {
+          case this.authService.WRONG_PASSWORD_CODE:
+            this.snackbarService.show('🤔 Password incorrect !');
+            break;
+          case this.authService.USER_NOT_FOUND_CODE:
+            this.snackbarService.show('Account not found, create one ! 💪');
+            break;
+        }
+      });
   }
 
   private initSignInForm() {
