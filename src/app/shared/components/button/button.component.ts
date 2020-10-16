a8271216210d3f0f68638bdcd6d269796431c27d
Vincent Laude <vincent.laude2@gmail.com>
2020-10-16 18:59:04 +0200

    improvement(button): add type and fullWidth attribute

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -9,14 +9,17 @@ type color = 'default' | 'primary' | 'secondary' | 'danger';
 })
 // TODO bind button click to component click
 export class ButtonComponent implements OnInit {
+  @Input() type: 'button' | 'submit' = 'button';
   @Input() color: color = 'default';
   @Input() disabled: boolean;
   @Input() icon: boolean;
+  @Input() fullWidth: boolean;
 
   constructor() {}
 
   ngOnInit(): void {
     this.disabled = this.disabled !== undefined;
     this.icon = this.icon !== undefined;
+    this.fullWidth = this.fullWidth !== undefined;
   }
 }
