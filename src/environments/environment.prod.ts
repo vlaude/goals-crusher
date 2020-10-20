956a6fd537c34962d5793fd6b611d373d18f7dea
Vincent Laude <vincent.laude2@gmail.com>
2020-10-20 21:35:18 +0200

    config: add firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDJP4dosV0-MQ0mKUiwJe87eEbL7gmNqkU',
+    authDomain: 'goals-crusher-13ccd.firebaseapp.com',
+    databaseURL: 'https://goals-crusher-13ccd.firebaseio.com',
+    projectId: 'goals-crusher-13ccd',
+    storageBucket: 'goals-crusher-13ccd.appspot.com',
+    messagingSenderId: '807076644137',
+    appId: '1:807076644137:web:75e4896135c7d0c3f06338',
+    measurementId: 'G-R0RZG5NQM2',
+  },
 };
