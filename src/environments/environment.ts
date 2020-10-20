6b4e4452dc14ed3414cfdb08122e023814e13a06
Vincent Laude <vincent.laude2@gmail.com>
2020-10-20 21:35:18 +0200

    config: add firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDJP4dosV0-MQ0mKUiwJe87eEbL7gmNqkU',
+    authDomain: 'goals-crusher-13ccd.firebaseapp.com',
+    databaseURL: 'https://goals-crusher-13ccd.firebaseio.com',
+    projectId: 'goals-crusher-13ccd',
+    storageBucket: 'goals-crusher-13ccd.appspot.com',
+    messagingSenderId: '807076644137',
+    appId: '1:807076644137:web:75e4896135c7d0c3f06338',
+    measurementId: 'G-R0RZG5NQM2',
+  },
 };
 
 /*
