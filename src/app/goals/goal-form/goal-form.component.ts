03c7613d3c3bd7a96535f85e9bdcc5104c1b1306
Vincent Laude <vincent.laude2@gmail.com>
2020-10-22 22:29:43 +0200

    feat: add new goal-form component

---
+++ src/app/goals/goal-form/goal-form.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, EventEmitter, Output, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RadioButtonItem } from '../../shared/components/radio-button/radio-button.component';
+import { GoalModel } from '../../core/models/goal.model';
+
+@Component({
+  selector: 'vl-goal-form',
+  templateUrl: './goal-form.component.html',
+  styleUrls: ['./goal-form.component.scss'],
+})
+export class GoalFormComponent implements OnInit, OnChanges {
+  @Input() goal: GoalModel<any>;
+  @Output() formSubmitted = new EventEmitter<FormGroup>();
+  @Output() cancelBtnClicked = new EventEmitter<void>();
+
+  public form: FormGroup;
+  goalTypesItems: RadioButtonItem[];
+
+  get titleFormControl(): AbstractControl {
+    return this.form.controls.title;
+  }
+  get descriptionFormControl(): AbstractControl {
+    return this.form.controls.description;
+  }
+
+  constructor(private readonly fb: FormBuilder) {
+    this.buildForm();
+  }
+
+  ngOnInit(): void {
+    this.goalTypesItems = [
+      { name: 'Daily', value: 'daily' },
+      { name: 'Weekly', value: 'weekly' },
+      { name: 'Lifelong', value: 'lifelong' },
+    ];
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.goal.currentValue !== changes.goal.previousValue) {
+      this.buildForm();
+    }
+  }
+
+  private buildForm() {
+    this.form = this.fb.group({
+      id: [this.goal?.id || null],
+      title: [this.goal?.title || '', [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+      description: [this.goal?.description || '', Validators.maxLength(250)],
+      type: [this.goal?.type || 'daily', Validators.required],
+    });
+  }
+
+  submit() {
+    if (!this.form.valid) return;
+    this.formSubmitted.emit(this.form);
+  }
+
+  cancel() {
+    this.cancelBtnClicked.emit();
+  }
+}
