6b05ea2aadc5353ac2e318eb3ef1500bcd386a9e
Vincent Laude <vincent.laude2@gmail.com>
2020-10-31 12:21:46 +0100

    refactor(shared): remove unused components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GoalListComponent } from './components/goal-list/goal-list.component';
-import { GoalDetailDialogComponent } from './components/goal-detail-dialog/goal-detail-dialog.component';
-import { GoalFormDialogComponent } from './components/goal-form-dialog/goal-form-dialog.component';
 import { MaterialModule } from '../material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TimeLeftPipe } from './pipes/time-left.pipe';
-import { AchieveGoalDialogComponent } from './components/achieve-goal-dialog/achieve-goal-dialog.component';
-import { UnachieveGoalDialogComponent } from './components/unachieve-goal-dialog/unachieve-goal-dialog.component';
 import { ModalComponent } from './components/modal/modal.component';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
@@ -22,11 +18,7 @@ import { SnackbarComponent } from './components/snackbar/snackbar.component';
 @NgModule({
   declarations: [
     GoalListComponent,
-    GoalDetailDialogComponent,
-    GoalFormDialogComponent,
     TimeLeftPipe,
-    AchieveGoalDialogComponent,
-    UnachieveGoalDialogComponent,
     ModalComponent,
     ButtonComponent,
     InputComponent,
