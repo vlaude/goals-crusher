8cf9d06049651e9322ee4c9924f3ff9e8e8bd4bc
Vincent Laude <vincent.laude2@gmail.com>
2020-11-10 17:48:04 +0100

    feat(login): refont login component + add login guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,21 +3,31 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './core/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './core/home/home.component';
-import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+import { AngularFireAuthGuard, redirectUnauthorizedTo, redirectLoggedInTo } from '@angular/fire/auth-guard';
 import { StateInitializedResolver } from './core/resolvers/state-initialized.resolver';
 import { ProfileContainerComponent } from './profile/profile-container/profile-container.component';
 import { GoalsContainerComponent } from './goals/goals-container.component';
 import { CalendarsContainerComponent } from './calendars/calendars-container.component';
 import { GoalsTabsComponent } from './goals/goals-tabs/goals-tabs.component';
 import { SettingsComponent } from './settings/settings.component';
+import { RegisterComponent } from './core/register/register.component';
 
 const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectLoggedInToDashboard = () => redirectLoggedInTo(['dashboard']);
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToDashboard },
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToDashboard },
   },
   {
     path: '',
