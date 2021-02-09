d62c03fdc5a19a37fc1b707112f97d75b8b5135e
Vincent Laude <vincent.laude2@gmail.com>
2021-02-09 19:04:57 +0100

    feat: change email

--- src/app/profile/components/email-edit/email-edit.component.ts
+++ src/app/profile/components/email-edit/email-edit.component.ts
@@ -1,9 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { UserModel } from '../../../core/models/user.model';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { StorageService } from '../../../core/services/storage.service';
-import { Observable } from 'rxjs';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { forbiddenEmailValidator } from '../../../shared/directives/forbidden-email.directive';
 
 @Component({
   selector: 'vl-email-edit',
@@ -13,11 +11,6 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class EmailEditComponent implements OnInit, OnChanges {
   @Input() user: UserModel;
   @Output() formSubmitted = new EventEmitter<FormGroup>();
-  @Output() cancelBtnClicked = new EventEmitter<void>();
-
-  fileToUpload: File;
-  avatarPreview: string | ArrayBuffer;
-  uploadProgress$: Observable<number>;
 
   form: FormGroup;
 
@@ -25,11 +18,7 @@ export class EmailEditComponent implements OnInit, OnChanges {
     return this.form.controls.email;
   }
 
-  constructor(
-    private readonly fb: FormBuilder,
-    private readonly storageService: StorageService,
-    private readonly afAuth: AngularFireAuth
-  ) {
+  constructor(private readonly fb: FormBuilder) {
     this.buildForm();
   }
 
@@ -42,53 +31,16 @@ export class EmailEditComponent implements OnInit, OnChanges {
   }
 
   async submit(): Promise<void> {
-    // if (!this.form.valid) {
-    //   return;
-    // }
-
-    const mediaFolderPath = `${this.user.email}/media/`;
-    const { downloadUrl$, uploadProgress$ } = this.storageService.uploadFileAndGetMetadata(
-      mediaFolderPath,
-      this.fileToUpload
-    );
-
-    this.uploadProgress$ = uploadProgress$;
-
-    downloadUrl$.subscribe(async (downloadUrl) => {
-      console.log('COOL !', downloadUrl);
-      (await this.afAuth.currentUser).updateProfile({ photoURL: downloadUrl });
-    });
-
-    // this.formSubmitted.emit(this.form);
-  }
-
-  cancel(): void {
-    this.cancelBtnClicked.emit();
-  }
-
-  handleFileChange(event: any): void {
-    if (event.target.files && event.target.files[0]) {
-      [this.fileToUpload] = event.target.files;
-      const reader = new FileReader();
-      reader.onload = (e: any) => {
-        this.avatarPreview = e.target.result;
-      };
-      reader.readAsDataURL(event.target.files[0]);
+    if (!this.form.valid) {
+      return;
     }
+
+    this.formSubmitted.emit(this.form);
   }
 
   private buildForm(): void {
     this.form = this.fb.group({
-      email: [this.user?.email || '', [Validators.required, Validators.email]],
-      avatar: [null, [Validators.required, this.image.bind(this)]],
+      email: [null, [Validators.required, Validators.email, forbiddenEmailValidator(this.user?.email)]],
     });
-  }
-
-  private image(photoControl: AbstractControl): { [key: string]: boolean } | null {
-    if (photoControl.value) {
-      const avatar = photoControl.value;
-      return null;
-    }
-    return;
   }
 }
